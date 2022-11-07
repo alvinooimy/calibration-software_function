9c5da976bb32e1275f8ce3116182c9687cf53863
alvinooimy <alvinooimy@gmail.com>
2022-11-07 23:00:37 +0800

    update findpeak2.py

--- auto_find_peaks/findpeaks2.py
+++ auto_find_peaks/findpeaks2.py
@@ -15,7 +15,10 @@ yData = np.asarray(df["I2"])
 
 plt.plot(xData, yData, 'b', label= "Raw Data")
 
-y_smooth = signal.savgol_filter(yData, window_length=9, polyorder=3, mode="nearest")
+#scipy.signal.savgol_filter(x, window_length, polyorder)
+#window_length即窗口长度。取值为奇数且不能超过len(x)。它越大，则平滑效果越明显；越小，则更贴近原始曲线。
+#polyorder为多项式拟合的阶数。它越小，则平滑效果越明显；越大，则更贴近原始曲线。
+y_smooth = signal.savgol_filter(yData, window_length = 21, polyorder = 4)
 plt.plot(xData, y_smooth, 'r', label= "SG Data")
 
 peaks, _ = signal.find_peaks(y_smooth, height = 0)
@@ -26,8 +29,8 @@ p_peaksmax_index1 = p_peaks.index(max(p_peaks))
 p_peaksmax1 = peaks[p_peaksmax_index1]
 p_peaks.pop(p_peaksmax_index1)
 
-p_peaksmax_index1 = p_peaks.index(max(p_peaks))
-p_peaksmax2 = peaks[p_peaksmax_index1]
+p_peaksmax_index2 = p_peaks.index(max(p_peaks))
+p_peaksmax2 = peaks[p_peaksmax_index2]
 
 if p_peaksmax1 > p_peaksmax2:
     hg_max = p_peaksmax1 + (p_peaksmax1 - p_peaksmax2)
@@ -67,7 +70,7 @@ for i in range(len(hg_peak1)):
     if i > 0 and i < 4:
         hg_peak2.append(hg_peak1[i])
 		
-plt.plot(hg_peak2, y_smooth[hg_peak2], 'r x', label = "hg-Peak")
+plt.plot(hg_peak2, y_smooth[hg_peak2], 'x', label = "hg-Peak", color = "black")
 
 
 plt.legend()	
